aecf124f163deeb57ddacc218ca04f794d633450
cazacuradu <cazacuraduic@gmail.com>
2022-09-17 11:18:01 +0300

    Init

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,68 @@
 #![recursion_limit = "512"]
 
+mod components;
+mod services;
+
+use components::chat::Chat;
+use components::login::Login;
 use wasm_bindgen::prelude::*;
 use yew::functional::*;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub type User = Rc<UserInner>;
+
+#[derive(PartialEq, Debug)]
+pub struct UserInner {
+    pub username: RefCell<String>,
+}
+#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+pub enum Route {
+    #[at("/")]
+    Login,
+    #[at("/chat")]
+    Chat,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(selected_route: &Route) -> Html {
+    match selected_route {
+        Route::Login => html! {<Login />},
+        Route::Chat => html! {<Chat />},
+        Route::NotFound => html! {<h1>{"404 baby"}</h1>},
+    }
+}
+
+#[function_component(Main)]
+fn main() -> Html {
+    let ctx = use_state(|| {
+        Rc::new(UserInner {
+            username: RefCell::new("initial".into()),
+        })
+    });
+
+    html! {
+            <ContextProvider<User> context={(*ctx).clone()}>
+        <BrowserRouter>
+            <div class="flex w-screen h-screen">
+                <Switch<Route> render={Switch::render(switch)} />
+            </div>
+        </BrowserRouter>
+                </ContextProvider<User>>
+    }
+}
+
+#[wasm_bindgen]
+pub fn run_app() -> Result<(), JsValue> {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Main>();
+    Ok(())
+}
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -12,4 +70,3 @@ use yew_router::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
